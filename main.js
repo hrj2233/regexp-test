8fa7cfadb6caac6b733e428f68e3671f9a03c46c
hrj2233 <hrj2233@gmail.com>
2022-06-08 03:04:18 +0900

    Create project

---
+++ main.js
@@ -0,0 +1,10 @@
+let str = `
+010-1234-5678
+thesecon@gmail.com
+https://www.omdbapi.com/?apikey=7035c60c&s=frozen
+The quick brown fox jumps over the lazy dog.
+abbcccdddd
+`;
+
+const regexp = /(?<=@).{1,}/g
+console.log(str.match(regexp));
